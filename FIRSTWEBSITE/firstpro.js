183829f04b0fc27e8476febd48975d20cbc5e556
bhatazhar <142919745+nuzhatniisar1238@users.noreply.github.com>
2023-10-05 20:04:44 -0700

    added.html

---
+++ FIRSTWEBSITE/firstpro.js
@@ -0,0 +1,13 @@
+var typed = new Typed(".text", {
+    strings: ["Frontend Web Developer", "freelancer", "Youtuber"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
+var typed = new Typed(".ok", {
+    strings: ["CodeHalal"],
+    typeSpeed: 200,
+    backSpeed: 100,
+    backDelay: 1000000000,
+});
